642baa80a4e602e71d2fc2e27fd5b23a0d1b5e59
mrgbpjpy <70776603+mrgbpjpy@users.noreply.github.com>
2023-05-17 00:39:08 -0400

    Commit message

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = 3001;
+
+const URI = 'mongodb+srv://vercel-admin-user:Ilovemywife123@cluster0.rmskjpq.mongodb.net/posts'
+
+app.use(cors({
+    allowedHeaders: ["Accept", "Content-Type"],
+  }));
+  
+app.use(express.json());
+
+//connect to mongoose
+mongoose.connect(URI);
+//require route
+app.use('/', (req, res)=>{
+ res.send("Home")
+});
+app.use('/', require('./routes/postRoute'));
+app.listen(port, () => {
+    console.log('Server is Running.');
+})
